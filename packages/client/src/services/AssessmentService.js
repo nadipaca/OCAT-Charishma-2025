706fe37b74278ec8d5989b0e9dad6fa1512231ad
nadipaca <nadipaca@mail.uc.edu>
2025-05-09 16:00:20 -0400

    Update service file to store form data in database

--- packages/client/src/services/AssessmentService.js
+++ packages/client/src/services/AssessmentService.js
@@ -1,12 +1,12 @@
 import Axios from '../utils/http.config';
 
 export class AssessmentService {
-  static submit(assessment) {
+  static async submit(assessment) {
     try {
       // Choose the correct method, url, and data to send
       // in a request to the express packages/api/src/routes/assessment.js
       // NOTE: the http.config file automatically adds /api to the front of your url
-      return Axios.METHOD(`/some-url`, { })
+      return await Axios.post(`/assessment`, assessment)
         .then(response => response.data);
     }
     catch (err) {
@@ -14,16 +14,16 @@ export class AssessmentService {
     }
   }
 
-  static getList() {
+  static async getList() {
     try {
       // Choose the correct method, url, and data to send
       // in a request to the express packages/api/src/routes/assessment.js
       // NOTE: the http.config file automatically adds /api to the front of your url
-      return Axios.METHOD(`/some-url`, {
+      return await Axios.get(`/assessment`, {
         params: {
         },
       })
-        .then(response => response.data.data.assessment);
+        .then(response => response.data.data.assessments);
     }
     catch (err) {
       throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
