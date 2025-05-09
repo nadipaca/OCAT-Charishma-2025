b3892e10fd94196a9ce99d7f94cf243323e5bbc1
nadipaca <nadipaca@mail.uc.edu>
2025-05-09 16:06:39 -0400

    add functionality in assesment service layer to create assesment in the table and also to fetch data from table in the ocat database

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -1,14 +1,19 @@
 const { Assessment } = require(`../database/models`);
 
 exports.submit = async (assessment) => {
+
   // use the sequelize model Assessments from packages/api/src/database/models to save
   // the assessment data in the PostgreSQL database
+
+  const savedAssessment = await Assessment.create(assessment);
+  return savedAssessment;
 };
 
-exports.getList = () => {
+exports.getList = async () => {
   // use the sequelize model Assessments from packages/api/src/database/models to fetch
   // the assessment data from the PostgreSQL database
-  const assessments = [];
-
+  const assessments = await Assessment.findAll({
+    order: [[ `id` ]],
+  });
   return assessments;
 };
