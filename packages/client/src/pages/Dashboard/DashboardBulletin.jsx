bc4e0fbfbf854efab8955bc96fbff244c250d9f0
nadipaca <nadipaca@mail.uc.edu>
2025-05-09 16:09:19 -0400

    import assesment list table componnet into the dashboard to display

--- packages/client/src/pages/Dashboard/DashboardBulletin.jsx
+++ packages/client/src/pages/Dashboard/DashboardBulletin.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { AssessmentList } from '../Assessments/AssessmentList';
 
 export const DashboardBulletin = () =>
   <div>
     <h1>OCAT Dashboard</h1>
     <hr />
+    <AssessmentList />
   </div>;
